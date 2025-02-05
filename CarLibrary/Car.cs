3cc0709516df1a8830cec4263c6d0fc506e95abc
Sakleaw2 <merbogdanodincov5@gmail.com>
2025-02-06 00:48:52 +0300

    изменены

--- CarLibrary/Car.cs
+++ CarLibrary/Car.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 namespace CarLibrary
 {
     // Представляет состояние двигателя.
     public enum EngineState { engineAlive, engineDead }
+    public enum MusicMedia
+    {
+        musicCd, // 0
+        musicTape, // 1
+        musicRadio, // 2
+        musicMp3 // 3
+    }
     // Абстрактный базовый класс в иерархии.
     public abstract class Car
     {
@@ -25,6 +34,28 @@ namespace CarLibrary
             PetName = name;
             MaxSpeed = maxSp;
             CurrentSpeed = currSp;
+        }
+        public void TurnOnRadio(bool musicOn, MusicMedia mm)
+        {
+            if (musicOn) MessageBox.Show(string.Format("Jamming {0}", mm));
+            else MessageBox.Show("Quiet time...");
+        }
+        static void InvokeMethodWithArgsUsingLateBinding(Assembly asm)
+        {
+            try
+            {
+                // Сначала получим метаданные для типа SportsCar.
+                Type sport = asm.GetType("CarLibrary.SportsCar");
+                // Затем создадим экземпляр типа SportsCar динамически.
+                object obj = Activator.CreateInstance(sport);
+                // Вызов метода TurnOnRadio() с аргументами.
+                MethodInfo mi = sport.GetMethod("TurnOnRadio");
+                mi.Invoke(obj, new object[] { true, 2 });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
